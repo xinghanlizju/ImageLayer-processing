895c0dbdaf44db83cd370ae69f78f55968d69fb7
admin <1515095368@qq.com>
2019-08-12 12:02:04 +0800

    template6的修改和输出路径更改

--- imagelayer.py
+++ imagelayer.py
@@ -8,10 +8,17 @@ def imgadd(fgImg, bgImg, fgPosRatio, fgSize, bgSize=1080, dir=0):
     img_1=fgImg
     img_2=bgImg
 
+    if img_1.shape[2] == 3:
+        img_1 = cv2.cvtColor(img_1, cv2.COLOR_BGR2BGRA)
+    if img_2.shape[2] == 3:
+        img_2 = cv2.cvtColor(img_2, cv2.COLOR_BGR2BGRA)
+
+
     #image normalizatiom
     img_1_resize=cv2.resize(img_1, (int(img_1.shape[1]*fgSize/img_1.shape[0]), fgSize), interpolation=cv2.INTER_AREA)
     img_2_resize=cv2.resize(img_2, (int(img_2.shape[1]*bgSize/img_2.shape[0]), bgSize), interpolation=cv2.INTER_AREA)
-
+    print('good',img_2_resize.shape)
+    print('bad', img_1_resize.shape)
 
     rows, cols, channels = img_1_resize.shape
     #Pos ratio convert into Pos
@@ -24,23 +31,60 @@ def imgadd(fgImg, bgImg, fgPosRatio, fgSize, bgSize=1080, dir=0):
 
     # img_text_pos=[bgSize*0.75, bgSize*bgRatio*0.8485]
     #create a ROI
-    roi = img_2_resize[int(fgPos[0]):int(fgPos[0]+rows), int(fgPos[1]):int(fgPos[1]+cols)]
+    print('fgPos[0]',fgPos[0])
+    print('fgPos[1]',fgPos[1])
+    #fgPos[0],点的行位置，
+    #fgPos[1]点的列位置，img_2_resize.shape[0]背景图片的长度，[1]宽度，cols指的是前景的长，rows指的是前景的宽
+    #前景图片超出背景图片的右部分
+    if fgPos[1]+cols >= img_2_resize.shape[1] and fgPos[0]+rows <= img_2_resize.shape[0]:
+        fgPos[1] = img_2_resize.shape[1]-cols
+
+    #前景图片超出背景图片的左部分
+    if fgPos[1]< 0 and fgPos[0]+rows <= img_2_resize.shape[0]:
+        fgPos[1] = max(0,fgPos[1])
+
+    #当前景图片超出背景图片下部分
+    if fgPos[0]+rows >= img_2_resize.shape[0] and fgPos[1]+cols <= img_2_resize.shape[1]:
+        fgPos[0] = img_2_resize.shape[0]-rows
+
+
+
+
+    #当前景照片超出背景图片的上部分，一般不可能发生
+    # roi = img_2_resize[max(int(fgPos[0]), 0):min(int(fgPos[0]+rows), img_2_resize.shape[0]-1), max(int(fgPos[1]), 0):min(int(fgPos[1]+cols), img_2_resize.shape[1]-1)]
+
+    roi = img_2_resize[max(int(fgPos[0]),0):max(int(fgPos[0]),0) + rows, max(int(fgPos[1]),0):max(int(fgPos[1] ),0)+ cols]
+    print(int(fgPos[1]), roi.shape)
+
     #create a mask of logo and create its inverse mask
     if channels > 3:
         b,g,r,alpha = cv2.split(img_1_resize)
         img_1_resize = cv2.merge([b,g,r])
         mask = alpha
         mask_inv = cv2.bitwise_not(mask)
+        # mask_final= tuple()
         # black-out the area in ROI
+        # print('mask_inv',mask_inv)
+        print('roi.shape',roi.shape)
+        print('mask_inv.shape',mask_inv.shape)
+
+        # mask_final = mask_inv.resize(min(roi.shape[0],mask_inv.shape[0]),min(roi.shape(1),mask_inv.shape(1)))
         img2_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
 
         # Take only region of foreground from foreground image.
         img1_fg = cv2.bitwise_and(img_1_resize, img_1_resize, mask=mask)
         # Put logo in ROI and modify the main image
+
+        if img2_bg.shape[2] == 3:
+            img2_bg = cv2.cvtColor(img2_bg, cv2.COLOR_BGR2BGRA)
+        if img1_fg.shape[2] == 3:
+            img1_fg = cv2.cvtColor(img1_fg, cv2.COLOR_BGR2BGRA)
+
         dst = cv2.add(img2_bg, img1_fg)
     else:
         dst = cv2.addWeighted(img_1_resize,1,roi,0,0)
-    img_2_resize[int(fgPos[0]):int(fgPos[0]+rows), int(fgPos[1]):int(fgPos[1]+cols)] = dst
+    img_2_resize[max(int(fgPos[0]), 0):max(int(fgPos[0]), 0) + rows, max(int(fgPos[1]),0):max(int(fgPos[1]),0) + cols]=dst
+    # img_2_resize[int(fgPos[0]):int(fgPos[0]+rows), int(fgPos[1]):int(fgPos[1]+cols)] = dst
     return img_2_resize
 
 #不同透明度图片叠加（还需要考虑png图像）
@@ -260,4 +304,3 @@ def complement_image(iname, oname):
     out_img = Image.new(mode, size)
     out_img.putdata([complement(*rgb) for rgb in in_data])
     out_img.save(oname)
-
